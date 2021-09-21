fb07c5da7e466a7032a958dbd604fc90348f6429
Bartosz Chrachol <bartekchrachol@wp.pl>
2021-09-21 07:55:53 +0200

    zmiana moze

--- test.js
+++ test.js
@@ -9,4 +9,4 @@ y('honda preloud123')
 //amonguas
 //asdkadjgasjdcgvsqfghjsavdcfhqgfusqgfgwqe\
 //swdf;hsadfjbaxc
-//\qfjksajcskjdf\
+//\qfjksajcskjdf\ zmiana tak  moze czasami ewentualnie w rzadkich przypadkach
